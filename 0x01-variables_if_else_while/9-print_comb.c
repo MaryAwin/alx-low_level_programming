dcc0f31f10bd741ae71cdbdb3b49aaa3cb15cb4f
maryawin <odongomaryawino2000@gmail.com>
2023-02-20 10:47:23 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - printing numbers from 0-9 with commas and space between them
- * Description: using the main function
- * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * main - main blo
+ * Description: prints all single number of base 10
+ * Starting from 0, followed by a new line
  * Return: 0
  */
 int main(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
+	int c = 0;
+while (c < 10)
 {
-putchar(c);
-if (c != '9')
+	putchar(48 + c);
+if (c != 9)
 {
 putchar(',');
-putchar(' ');
+putchar(',');
 }
-}
+c++;
 putchar('\n');
 return (0);
 }
