ac38209768c90d746e035350c5c13e2ec4ed53ff
maryawin <odongomaryawino2000@gmail.com>
2023-02-22 12:02:17 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+lines(16 sloc) 255 Bytes
+
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table, starting with 0
+ */
+void times_table(void);
+
+int main(void)
+{
+	times_table();
+	return (0);
+}
+
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	printf("9 x %d = %d\n",  i, 9 * i);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,4 @@ void jack_bauer(void);
 void times_table(void);
 void print_to_98(int n);
 void jack_bauer(void);
+void times_table(void);
